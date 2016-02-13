3a50ff004fcd28e3c57c2a75f6814ff58469b749
Baptiste Courtois <b.courtois@criteo.com>
2016-02-13 11:42:33 +0100

    Fix metrics compilation

---
+++ src/metrics/DotnetCoreMocks/PerformanceCounter.cs
@@ -0,0 +1,18 @@
+﻿
+namespace metrics.DotnetCoreMocks
+{
+    internal class PerformanceCounter
+    {
+        internal string CounterName { get; set; }
+        internal double NextValue()
+        {
+            return 1.0d;
+        }
+
+        internal static void CloseSharedResources()
+        {
+
+        }
+
+    }
+}
