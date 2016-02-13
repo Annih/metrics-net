285ad405705cb9193b6bfb98b2f42bfe324f660d
Baptiste Courtois <b.courtois@criteo.com>
2016-02-13 11:47:09 +0100

    Add main entry point for Metrics.Tests

---
+++ src/metrics.Tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using NUnitLite;
+using System;
+using System.Reflection;
+
+namespace metrics.Tests
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return new AutoRun().Execute(typeof(Program).GetTypeInfo().Assembly, Console.Out, Console.In, args);
+        }
+    }
+}
